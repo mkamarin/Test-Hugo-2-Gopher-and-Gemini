f5f3bd07753e29a26ace80eafeb35dd154628468
Mike Marin <ww6mm@runbox.com>
2021-03-08 15:46:50 -0800

    Setting up basic framework

--- test-hugo-theme-console/test.sh
+++ test-hugo-theme-console/test.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 # Simple test of the Hugo 2 Gopher and Gemini project
+hugo2ggtheme=Hugo-2-Gopher-and-Gemini
 
-echo TODO in  $(pwd)
+rm -f -r -d  public-gg-sav
+
+## Clean up code
+if [ "$2" = "clean" ]; then
+    echo "cleaning up $(pwd)"
+
+    rm -f -r -d  public
+    rm -f -r -d  public-gg
+
+    exit
+fi
+
+echo "Working in: $(pwd)"
+
+mv -f public-gg public-gg-sav
+
+echo "Executing hugo base line"
+hugo
+echo
+
+themes/${hugo2ggtheme}/src/hugo2gg.py
+
--- test.sh
+++ test.sh
@@ -2,42 +2,52 @@
 # Simple test of the Hugo 2 Gopher and Gemini project
 
 hugo2ggtheme=Hugo-2-Gopher-and-Gemini
+path2hugo2ggtheme="$(pwd)/../${hugo2ggtheme}/"
 now=$(date +"%Y-%m-%d-(%H-%M-%S)")
-log="$(pwd)/logs/log-$now.log"
 
-echo Start $(date)       2>&1 | tee "$log"
-echo Starting in: $(pwd) 2>&1 | tee -a "$log"
-echo Log in: "$log"      2>&1 | tee -a "$log"
-echo                     2>&1 | tee -a "$log"
+log="$(pwd)/logs/log-${now}.log"
+err="$(pwd)/logs/err-${now}.log"
+
+## Clean up code
+if [ "$1" = "clean" ]; then
+    echo "cleaning up $(pwd)"
+    rm -f logs/*
+    for folder in $(ls -d test-hugo-*/) 
+    do
+        rm -f "${folder}themes/${hugo2ggtheme}"
+
+        pushd "${folder}" >/dev/null
+        ./test.sh "'${err}'" 'clean'
+        popd >/dev/null
+    done
+
+    exit
+fi
+
+## Execute the tests
+echo "Start $(date)"            2>&1 | tee    "${log}" "${err}"
+echo "Starting in: $(pwd)"      2>&1 | tee -a "${log}" "${err}"
+echo "Log in: ${log}"           2>&1 | tee -a "${log}" "${err}"
+echo                            2>&1 | tee -a "${log}" "${err}"
 
 for folder in $(ls -d test-hugo-*/) 
 do
-    echo "==================" 2>&1 | tee -a "$log"
-    echo processing "$folder" 2>&1 | tee -a "$log"
-    if ! [ -e "$folder"/themes"$hugo2ggtheme" ]; then
-        echo Creating "$folder"themes/"$hugo2ggtheme" 2>&1 | tee -a "$log"
-        #ln -s ../"$hugo2ggtheme"/ "$folder"themes/"$hugo2ggtheme"
-    fi
-    pushd "$folder" >/dev/null
-    echo Working in: $(pwd) 2>&1 | tee -a "$log"
-    ./test.sh               2>&1 | tee -a "$log"
-    popd >/dev/null
-    rm -f "$folder"themes/"$hugo2ggtheme"  2>&1 | tee -a "$log"
-    echo                                   2>&1 | tee -a "$log"
-done
-echo "==================" 2>&1 | tee -a "$log"
-echo                      2>&1 | tee -a "$log"
+    echo "=================="   2>&1 | tee -a "${log}" "${err}"
+    echo "Processing ${folder}" 2>&1 | tee -a "${log}" "${err}"
 
-# rm -f -r -d  test/public-gg-old
-# rm -f -r -d  test/themes-old
-# mv -f test/public-gg test/public-gg-old
-# mv -f test/themes test/themes-old
-# mkdir test/themes
-# cp -f -r themes/Gopher-Gemini test/themes
-# pushd test >/dev/null
-# #hugo -D --config config-gg.toml --destination public-gg --layoutDir layouts-gg --disableKinds=sitemap
-# themes/Gopher-Gemini/src/hugo2gg.py
-# #sshpass -p "KF6rqv+WW6mm" scp -r test/public-gg/gopher ubuntu@pi-server:public/gopher/ubuntu/mike
-# #sshpass -p "KF6rqv+WW6mm" scp -r test/public-gg/gemini ubuntu@pi-server:src/JAGS-PHP/hosts/mike
-# popd >/dev/null
-echo Done $(date) 2>&1 | tee -a "$log"
+    if ! [ -e "${folder}themes/${hugo2ggtheme}" ]; then
+        echo "Creating ${folder}themes/${hugo2ggtheme}" 2>&1 | tee -a "${log}"
+        ln -s "${path2hugo2ggtheme}" "${folder}themes/${hugo2ggtheme}"
+    fi
+
+    pushd "${folder}" >/dev/null
+    ./test.sh "'${err}'"        2>&1 | tee -a "${log}"
+    popd >/dev/null
+
+    echo                        2>&1 | tee -a "${log}" "${err}"
+done
+echo "=================="       2>&1 | tee -a "${log}"
+echo                            2>&1 | tee -a "${log}" "${err}"
+
+echo Done $(date)               2>&1 | tee -a "${log}" "${err}"
+
